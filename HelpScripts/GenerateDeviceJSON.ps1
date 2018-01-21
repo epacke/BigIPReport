22d81b53833ddb03ae2fbf8d2797a9ee40d4aed5
Indeni_PJ <patrik.jonsson@gmail.com>
2018-01-21 19:18:05 +0800

    Adding icons for LBs and json file containing devices.

---
+++ HelpScripts/GenerateDeviceJSON.ps1
@@ -0,0 +1,53 @@
+﻿
+#Based on the tables in https://support.f5.com/csp/article/K9476
+
+$F5HardwareMatrix = [xml](Get-Content C:\Temp\hw.html)
+
+$DeviceJSON = @{}
+
+Foreach($DeviceRow in $F5HardwareMatrix.div.table.tbody.tr){
+    
+    $DeviceCells = $DeviceRow.td
+
+    $Type = $DeviceCells[1]
+    $SoftwareVersion = $DeviceCells[2]
+
+    If(-not $DeviceJSON.ContainsKey($Type)){
+        $TypeDict = @{}
+        $TypeDict.add("softwareVersion", @())
+        $TypeDict.softwareVersion += $SoftwareVersion -Split ", "
+        $TypeDict.icon = ""
+        
+        #We're only interested in version 11
+        if(($TypeDict.softwareVersion -Match "(^11\.|- 11\.)").Count -gt 0){
+            $DeviceJSON.add($Type, $TypeDict)
+        }
+
+    }
+    
+}
+
+$F5HardwareMatrix = [xml](Get-Content C:\Temp\vpr.xml)
+
+Foreach($DeviceRow in $F5HardwareMatrix.div.table.tbody.tr){
+    
+    $DeviceCells = $DeviceRow.td
+
+
+    if($DeviceCells.Count -eq 5){
+        $ChassisTypeArr = $DeviceCells[0].a.'#text' -Split "/"
+    }
+
+    $Type = $ChassisTypeArr[1]
+
+    $TypeDict = @{}
+    $TypeDict.add("softwareVersion", @())
+    $TypeDict.icon = ""
+   
+    If(-not $DeviceJSON.ContainsKey($Type)){
+        $DeviceJSON.add($Type, $TypeDict)
+    }
+}
+
+
+ConvertTo-Json -Depth 5 $DeviceJSON | Out-File "C:\BigIPReport\Move the content of this folder to the configured report root\json\knowndevices.json"
