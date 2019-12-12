6f0b2f587698e6df26bc908c9cfb5b48578776bf
Tim Riker <Tim@Rikers.org>
2019-12-12 11:40:05 -0700

    more test code

--- HelpScripts/resttest.ps1
+++ HelpScripts/resttest.ps1
@@ -6,17 +6,51 @@ Param(
     $userpass
 )
 
-Set-StrictMode -Version 1.0
+Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $Error.Clear()
 
-$EncodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($userpass))
-$BasicAuthValue = "Basic $EncodedCreds"
+Function Get-AuthToken {
+    Param($Device,$userpass)
 
-$Headers = @{
-    "Authorization" = $BasicAuthValue
+    #Encode the string to base64
+    $EncodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($userpass))
+
+    #Add the "Basic prefix"
+    $BasicAuthValue = "Basic $EncodedCreds"
+
+    #Prepare the headers
+    $Headers = @{
+        "Authorization" = $BasicAuthValue
+        "Content-Type" = "application/json"
+    }
+
+    #Create the body of the post
+    $Body = @{"username" = $userpass.split(":")[0]; "password" = $userpass.split(":")[1]; "loginProviderName" = "tmos" }
+
+    #Convert the body to Json
+    $Body = $Body | ConvertTo-Json
+
+    try {
+        $Response = ""
+        $Response  = Invoke-RestMethod -Method "POST" -Headers $Headers -Body $Body -Uri "https://$Device/mgmt/shared/authn/login"
+    } catch {
+        Write-Host "Could not login as $userpass to $Device : $Response"
+        return $null
+    }
+
+    if ($Response.token.token) {
+        $Headers = @{ "X-F5-Auth-Token" = $Response.token.token; }
+        return $Headers
+    } else {
+        return $null
+    }
 }
 
+$Headers = Get-AuthToken $Device $userpass
+if (-not ($Headers)) {
+    exit
+}
 $Response1 = Invoke-RestMethod -Headers $Headers -Uri "https://$Device/mgmt/tm/ltm/pool/members/stats?`$filter=partition"
 
 Foreach($PoolStat in $Response1.entries.psobject.properties.Value) {
